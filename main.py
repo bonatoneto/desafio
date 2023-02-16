6b13cdb689c224ccff8e0a2b8ca7051e1de5799b
bonatoneto <joaoadolfobonato@gmail.com>
2023-02-16 03:04:14 -0300

    terceiro commit

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ def hello():
 def resultado():
     mensagem = "Esse é o resultado"
     nome = request.form.get('nome')
-    sobrenome = request.form.get('sobrenome')
-    nomeCompleto = nome + " " + sobrenome
-    return render_template('result.html'), nomeCompleto
+    numero = request.form.get('numero', type=int) or 0
+    telefone = request.form.get('tel', type=int)
+    email = request.form.get('email')
+    if numero % 3 == 0:
+        return render_template('result.html', nome=nome)
+    elif numero % 5 ==0:
+        return render_template('result.html', telefone=telefone)
+    elif numero % 7 ==0:
+        return render_template('result.html', email=email)
+    else:
+        return render_template('result.html', mensagem= mensagem)
 
 if __name__ == '__main__':
     app.run(debug=True)
