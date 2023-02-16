bd5e42fa834edcadfef1177887db1b4cb3d8656e
bonatoneto <joaoadolfobonato@gmail.com>
2023-02-15 21:11:07 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/inicio')
 def hello():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/resultado", methods=["POST"])
+@app.route('/resultado', methods=['POST'])
 def resultado():
-    mensagem = "esse é o resultado"
-    return mensagem
+    mensagem = "Esse é o resultado"
+    nome = request.form.get('nome')
+    sobrenome = request.form.get('sobrenome')
+    nomeCompleto = nome + " " + sobrenome
+    return render_template('result.html'), nomeCompleto
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
